ad7e3fc93f2b587fc9bf9e33d9337fad938e8a35
Markbeep <mrkswrn@gmail.com>
2023-04-12 01:03:53 +0200

    One line install everything

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,24 +1,14 @@
-# Mark's Personal NixOS Setup
-
-One line install everything:
-```bash
-curl https://raw.githubusercontent.com/markbeep/nixos-config/main/install.sh | sh
-```
-
-List of commands to copy the setup:
-
-```bash
-# temporarily install git in the shell
-nix-shell -p git
-
+# Script used to easily install everything needed in one go
 cd ~
+echo "Cloning repo"
 git clone https://github.com/markbeep/NixOS-config ~/nixos-config
 
 # add the unstable and home-manager channels
+echo "Adding channels"
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 
 # builds the main part using the custom configuration
+echo "Rebuilding system (requires root)"
 sudo nixos-rebuild switch -I $HOME/nixos-config/configuration.nix
-```
